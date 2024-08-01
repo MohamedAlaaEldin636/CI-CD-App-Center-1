c99f1a9a2a818fe40404434b2bb4ce846f192081
Mohamed Alaa <mohamed.alaa636@hotmail.com>
2024-08-01 14:57:34 +0300

    added app center dependency and label isa

--- app/src/androidTest/java/my/ym/cicdappcenter1/MainActivityTest1.kt
+++ app/src/androidTest/java/my/ym/cicdappcenter1/MainActivityTest1.kt
@@ -7,10 +7,15 @@ import androidx.compose.ui.test.performClick
 import my.ym.cicdappcenter1.ui.theme.CICDAppCenter1Theme
 import org.junit.Rule
 import org.junit.Test
+import com.microsoft.appcenter.espresso.Factory
+import com.microsoft.appcenter.espresso.ReportHelper
+import org.junit.After
 
 class MainActivityTest1 {
 
     @get:Rule val composeTestRule = createComposeRule()
+
+    @get:Rule val reportHelper: ReportHelper = Factory.getReportHelper()
 
     @Test
     fun myTest() {
@@ -26,6 +31,11 @@ class MainActivityTest1 {
         composeTestRule.onNodeWithText("Clicked 0").performClick()
 
         composeTestRule.onNodeWithText("Clicked 1").assertIsDisplayed()
+    }
+
+    @After
+    fun tearDown() {
+        reportHelper.label("Done isa.")
     }
 
 }
